ec9b8df77de7624014b7f1bac24d354aa247e6a9
CharkeyQK <charkeyqk@gmail.com>
2023-04-24 17:58:02 +0800

    feat(auth): auth enable

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { setupI18n } from './locales'
 import { setupAssets, setupScrollbarStyle } from './plugins'
 import { setupStore } from './store'
 import { setupRouter } from './router'
+import Keycloak from 'keycloak-js'
+import Vue from 'vue'
+
 
 async function bootstrap() {
   const app = createApp(App)
@@ -20,4 +23,39 @@ async function bootstrap() {
   app.mount('#app')
 }
 
-bootstrap()
+
+
+let initOptions = {
+  url: 'https://sso.dstp.com.cn/', realm: 'devops', clientId: 'chatgpt', onLoad: 'login-required'
+}
+
+let keycloak = new Keycloak(initOptions);
+
+keycloak.init({ onLoad: initOptions.onLoad }).then((auth) => {
+  if (!auth) {
+    window.location.reload();
+  } else {
+    console.log("bootstrap")
+    bootstrap()
+    setTimeout(() => {
+      window.location.href = window.location.origin + '/#/';
+    }, 1000)
+
+  }
+
+  //Token Refresh
+  setInterval(() => {
+    keycloak.updateToken(70).then((refreshed) => {
+      if (refreshed) {
+        console.log("refreshed")
+      } else {
+        console.log("refreshed else")
+      }
+    }).catch(() => {
+      console.log("refreshed exception catch")
+    });
+  }, 6000)
+
+}).catch(() => {
+  console.log("keycloak.init exception catch")
+});
